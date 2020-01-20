1864b6e193d4ed771b88e4441cd26f24aa88b341
Rohit Gautam <RohitGautam@apples-MacBook-Air.local>
2020-01-20 22:35:03 +0530

    First

---
+++ src/features/event/EventList/EventList.jsx
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react';
+import EventListItem from './EventListItem';
+
+class EventList extends Component {
+  render() {
+    return (
+      <Fragment>
+        <EventListItem />
+        <EventListItem />
+        <EventListItem />
+      </Fragment>
+    );
+  }
+}
+export default EventList;
